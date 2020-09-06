2eefc550433bfefa025f9af3e363318417814b23
Christian Medina <christianmedina121@hotmail.com>
2020-09-06 19:10:46 -0400

    Adding action bar menu items, logo, center titled and some buttons

--- Ticketing/app/src/main/java/com/example/ticketing/MainActivity.java
+++ Ticketing/app/src/main/java/com/example/ticketing/MainActivity.java
@@ -1,8 +1,20 @@
 package com.example.ticketing;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatTextView;
+import androidx.appcompat.widget.Toolbar;
 
+import android.os.Build;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +22,88 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Action bar setup
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setTitle("Libix");
+        //actionBar.setSubtitle("Welcome to the app");
+
+        centerTitle();
+
+        //Display logo
+        actionBar.setIcon(R.drawable.templogo);
+        actionBar.setDisplayUseLogoEnabled(true);
+        actionBar.setDisplayShowHomeEnabled(true);
+
+    }
+
+    //Displays the action bar created in main.xml
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        getMenuInflater().inflate(R.menu.main, menu);
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    //Specifies what each item does when clicked
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        switch(item.getItemId())
+        {
+            case R.id.notifications:
+                Toast.makeText(this, "Notifications clicked", Toast.LENGTH_SHORT).show();
+                break;
+
+            case R.id.account:
+                Toast.makeText(this, "Account clicked", Toast.LENGTH_SHORT).show();
+                break;
+
+            case R.id.info:
+                Toast.makeText(this, "Info clicked", Toast.LENGTH_SHORT).show();
+                break;
+
+            case R.id.update:
+                Toast.makeText(this, "Update clicked", Toast.LENGTH_SHORT).show();
+                break;
+
+            case R.id.logout:
+                Toast.makeText(this, "Logout clicked", Toast.LENGTH_SHORT).show();
+                break;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    //method for centering title without creating a custom action bar. Thanks THEPATEL on stackoverflow
+    private void centerTitle() {
+        ArrayList<View> textViews = new ArrayList<>();
+
+        getWindow().getDecorView().findViewsWithText(textViews, getTitle(), View.FIND_VIEWS_WITH_TEXT);
+
+        if(textViews.size() > 0) {
+            AppCompatTextView appCompatTextView = null;
+            if(textViews.size() == 1) {
+                appCompatTextView = (AppCompatTextView) textViews.get(0);
+            } else {
+                for(View v : textViews) {
+                    if(v.getParent() instanceof Toolbar) {
+                        appCompatTextView = (AppCompatTextView) v;
+                        break;
+                    }
+                }
+            }
+
+            if(appCompatTextView != null) {
+                ViewGroup.LayoutParams params = appCompatTextView.getLayoutParams();
+                params.width = ViewGroup.LayoutParams.MATCH_PARENT;
+                appCompatTextView.setLayoutParams(params);
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR1) {
+                    appCompatTextView.setTextAlignment(View.TEXT_ALIGNMENT_CENTER);
+                }
+            }
+        }
     }
 }
