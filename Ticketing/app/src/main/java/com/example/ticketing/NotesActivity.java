af65b7b96c1c60eaedd593962a8ea7c79a23dcce
Christian Medina <christianmedina121@hotmail.com>
2020-09-10 00:31:59 -0400

    Styling child activites

--- Ticketing/app/src/main/java/com/example/ticketing/NotesActivity.java
+++ Ticketing/app/src/main/java/com/example/ticketing/NotesActivity.java
@@ -4,7 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class NotesActivity extends AppCompatActivity {
+public class NotesActivity extends MainActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
