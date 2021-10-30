59e8dd426464aeb60ea4e40ba2252f732c9823bc
Christian Medina <christianmedina121@hotmail.com>
2021-10-30 19:32:37 -0400

    New Recycler View files.Forgot to add them in the previous commit.

---
+++ Ticketing/app/src/main/java/com/example/ticketing/TicketAdapter.java
@@ -0,0 +1,64 @@
+package com.example.ticketing;
+
+import android.content.Context;
+import android.text.Layout;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+public class TicketAdapter extends RecyclerView.Adapter<TicketAdapter.TicketViewHolder> {
+
+    ArrayList <TroubleTicket> tickets = new ArrayList<TroubleTicket>();
+    Context context;
+
+    public TicketAdapter(Context context, ArrayList<TroubleTicket> tickets){
+        this.context = context;
+        this.tickets = tickets;
+    }
+    @NonNull
+    @Override
+    public TicketAdapter.TicketViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutInflater inflater = LayoutInflater.from(context);
+        View view = inflater.inflate(R.layout.ticket, parent, false);
+
+        return new TicketViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull TicketAdapter.TicketViewHolder holder, int position) {
+
+        holder.subjectView.setText(tickets.get(position).subject);
+        holder.statusView.setText(tickets.get(position).status);
+        holder.descriptionView.setText(tickets.get(position).body);
+    }
+
+    @Override
+    public int getItemCount() {
+        try {
+            return tickets.size();
+        } catch (Exception e){
+            return 0;
+        }
+    }
+
+    public class TicketViewHolder extends RecyclerView.ViewHolder {
+
+        TextView subjectView;
+        TextView statusView;
+        TextView descriptionView;
+
+        public TicketViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            subjectView = itemView.findViewById(R.id.ticket_subject);
+            statusView = itemView.findViewById(R.id.ticket_status);
+            descriptionView = itemView.findViewById(R.id.ticket_description);
+        }
+    }
+}
