a23a51571ac2b277a23828ee30e8600abf4d3121
Christian Medina <christianmedina121@hotmail.com>
2022-05-31 17:42:26 -0400

    Updated gitignore. Added untracked files

---
+++ Ticketing/app/src/main/java/com/example/ticketing/PushNotificationSender.java
@@ -0,0 +1,79 @@
+package com.example.ticketing;
+
+import android.app.Activity;
+import android.content.Context;
+import android.util.Log;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class PushNotificationSender  {
+
+    String userFcmToken;
+    String title;
+    String body;
+    Context mContext;
+    Activity mActivity;
+
+
+    private RequestQueue requestQueue;
+    private final String postUrl = "https://fcm.googleapis.com/fcm/send";
+    private final String fcmServerKey = Config.FCMSERVERKEY;
+
+    public PushNotificationSender(String userFcmToken, String title, String body, Context mContext, Activity mActivity) {
+        this.userFcmToken = userFcmToken;
+        this.title = title;
+        this.body = body;
+        this.mContext = mContext;
+        this.mActivity = mActivity;
+    }
+
+    public void sendNotifications() {
+
+        requestQueue = Volley.newRequestQueue(mActivity);
+        JSONObject mainObj = new JSONObject();
+        try {
+            mainObj.put("to", userFcmToken);
+            JSONObject notificationObject = new JSONObject();
+            notificationObject.put("title", title);
+            notificationObject.put("body", body);
+            notificationObject.put("icon", R.drawable.notifications);
+
+            mainObj.put("notification", notificationObject);
+
+            JsonObjectRequest request = new JsonObjectRequest(Request.Method.POST, postUrl, mainObj, new Response.Listener<JSONObject>() {
+                @Override
+                public void onResponse(JSONObject response) {
+                    Log.d("NotificationResponse", response.toString());
+                }
+            }, new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(VolleyError error) {
+                    Log.d("PushNotificationError", "Error sending push notification");
+                }
+            }) {
+                @Override
+                public Map<String, String> getHeaders() throws AuthFailureError {
+                    Map<String, String> header = new HashMap<>();
+                    header.put("content-type", "application/json");
+                    header.put("authorization", "key=" + fcmServerKey);
+                    return header;
+                }
+            };
+            requestQueue.add(request);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+    }
+}
