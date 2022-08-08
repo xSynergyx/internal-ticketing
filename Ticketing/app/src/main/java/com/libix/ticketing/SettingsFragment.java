69de64514c57c13a5fb8e7baafd7547f865b49c3
Christian Medina <christianmedina121@hotmail.com>
2022-08-08 07:33:14 -0400

    Added a SharedPreference for notifications.

---
+++ Ticketing/app/src/main/java/com/libix/ticketing/SettingsFragment.java
@@ -0,0 +1,34 @@
+package com.libix.ticketing;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.widget.Toast;
+
+import androidx.preference.PreferenceFragmentCompat;
+
+import com.google.firebase.messaging.FirebaseMessaging;
+import com.microsoft.graph.models.extensions.Shared;
+
+public class SettingsFragment extends PreferenceFragmentCompat implements SharedPreferences.OnSharedPreferenceChangeListener {
+
+    @Override
+    public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
+        setPreferencesFromResource(R.xml.root_preferences, rootKey);
+
+        SharedPreferences sharedPreferences = getContext().getSharedPreferences("notifications", Context.MODE_PRIVATE);
+        sharedPreferences.registerOnSharedPreferenceChangeListener(this);
+
+    }
+
+    @Override
+    public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key){
+        if (key.equals("notifications") && sharedPreferences.getBoolean("notifications",true)){
+            FirebaseMessaging.getInstance().subscribeToTopic("all");
+            Toast.makeText(getContext(), "Subscribed to notifications", Toast.LENGTH_SHORT).show();
+        } else if (key.equals("notifications")){
+            Toast.makeText(getContext(), "Unsubscribed from notifications", Toast.LENGTH_SHORT).show();
+            FirebaseMessaging.getInstance().subscribeToTopic("all");
+        }
+    }
+}
